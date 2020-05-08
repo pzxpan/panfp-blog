4003e704653c970bab51dc980a5c4c38db3ad197
潘振兴 <panzhenxing551@gmail.com>
2020-05-08 13:51:42 +0800

    添加打包配置文件

---
+++ .umirc.js
@@ -0,0 +1,78 @@
+export default {
+  disableCSSModules: true,
+  treeShaking: true,
+  routes: [
+    {
+      path: '/user',
+      component: '../layouts/UserLayout',
+      routes: [
+        {
+          path: '/user',
+          redirect: '/user/login',
+        },
+        {
+          path: '/user/login',
+          name: 'login',
+          component: './user/login',
+        },
+        {
+          path: '/user/register',
+          name: 'register',
+          component: './user/register',
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      path: '/article',
+      component: '../layouts/ArticleLayout',
+      routes: [
+        {
+          path: '/article/:id',
+          name: 'detail',
+          component: './article/article',
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: '../layouts/BasicLayout',
+      routes: [
+        {
+          path: '/',
+          name: 'home',
+          component: './home/home',
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'Blog',
+      dll: false,
+
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+
+}
